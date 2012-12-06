e4a058b85c7d72dccc2bdabb2f9ecfd0a1237163
Masahiro Nishimi <nishimi@sonicgarden.jp>
2012-12-06 14:45:27 +0900

    config.phonegap_path is unnecessary

--- lib/kanna.rb
+++ lib/kanna.rb
@@ -1,12 +1,5 @@
 require "kanna/version"
-require "active_support/configurable"
 
 module Kanna
-  include ActiveSupport::Configurable
-
-  configure do |config|
-    config.phonegap_path = File.join('~', 'install', 'phonegap-2.0.0')
-  end
-
   require "kanna/railtie"
 end
